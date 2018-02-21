3d3ba60148e81c2b208b94aa26d9ef4740c37539
Brobelia <brandanlemovieman@yahoo.com>
2018-02-21 13:07:23 -0600

    generation edit
    Small tweaks here and there to try and make it easier to implement into the other classes

--- generate.h
+++ generate.h
@@ -1,28 +1,24 @@
 #pragma once
 
-#include <stdio.h>
-#include <math.h>
 #include <vector>
-#include <ctime>
-#include <cstdlib>
-#include <iostream>
-#include "stdafx.h"
 #include "Ore.h"
 #include "Tile.h"
+
+using namespace std;
 
 class Generation
 {
 public:
-	vector < vector<Tile> > createTileGrid(int xAxis, int yAxis);
-	vector < vector<Ore> > createOreGrid(int xAxis, int yAxis);
+	vector < vector<Tile> > createTileGrid(int xAxis, int yAxis, Tile til);
+	vector < vector<Ore> > createOreGrid(int xAxis, int yAxis, Ore oreo);
 	void showTiles(vector < vector <Tile> > tiles);
 	void lakeAlGore(vector < vector <Tile> > &tiles);
-	void orAlGore(vector < vector <Ore> > ores, vector < vector <Tile> > &tiles);
+	void oreAlGore(vector < vector <Ore> > &ores, vector < vector <Tile> > &tiles);
 	void revampOre(vector < vector <Ore> > &ores);
 private:
 	Tile til;
 	Ore ore;
-	int xAxis;
+	int xAxis; //may be useless and rely on TileMap's SIZE_X number instead 
 	int yAxis;
 
 };
