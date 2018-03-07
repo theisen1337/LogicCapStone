2b745d6f6c8017107a72f075272c0b86df34dfda
theisen1337 <theist7901@my.uwstout.edu>
2018-03-07 14:41:34 -0600

    :hammer: Dustin Push

--- DevSprint2/StateManager.h
+++ DevSprint2/StateManager.h
@@ -13,13 +13,14 @@
 #include "World.h"
 #include "Interact.h"
 #include "FileIO.h"
-//#include "Items.h"
-//#include "ItemsLayer.h"
-//#include "Ore.h"
-//#include "Tile.h"
-//#include "Artist.h"
 #include "MainDraw.h"
 #include "ObjectManager.h"
+#include "MainDraw.h"
+#include "MainCompute.h"
+#include "MachineLayer.h"
+#include "TransportLayer.h"
+#include "CharacterPlayer.h"
+#include "transportTemplate.h"
 #include "GlobalStatics.h"
 
 using namespace std;
@@ -28,14 +29,38 @@ using namespace std;
 class StateManager
 {
 	public:
+		ObjectManager ObjMang;
+		GlobalStatics GlobStat;
+		Interact Interactions;
+		World Map;
+		Interact interactions;
+		MainDraw mainDraw;
+		MainCompute mainCompute;
+		CharacterPlayer player;
 
+		// Window Used for Display
+		ALLEGRO_DISPLAY *display;
+		// Tilemap Test Image
+		ALLEGRO_BITMAP *BlahtestImage;
+		// Custom Font
+		ALLEGRO_FONT *font;
+
+		ALLEGRO_EVENT_QUEUE *queue;
+
+		ALLEGRO_TRANSFORM transform;
+		float w, h;
+
+		ALLEGRO_TIMER *timer;
+
+		void Transformations();
 		void run();
-		
+		void Initialization();
 		void Computing();
 		void Drawing();
 		void Interacting();
 		void Exiting();
 
+		bool GAMERUN = true;
 
 		void MainLoop(); //World &Map, Artist &Art, ALLEGRO_DISPLAY * display, ALLEGRO_FONT * font
 };
