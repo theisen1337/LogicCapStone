73205d7ffbbd466ceb100aa69e4b003d1a0eddde
Taylor Thomas <martinw7724@gmail.com>
2018-03-21 09:58:37 -0500

    Commenting Improvements
    Redid the Comments and Added More Depth to the Descriptions

--- DevSprint1/Interact.cpp
+++ DevSprint1/Interact.cpp
@@ -1,70 +1,83 @@
 #include "Interact.h"
 
-//World Map = *(LogisticsGame::getMap());
-
-Interact::Interact() { }
-
+// ####################
+// # Create our Event #
+// ####################
 ALLEGRO_EVENT event;
 
+// ###############
+// # Constructor #
+// ###############
+Interact::Interact() {}
+
+// #################
+// # Deconstructor #
+// #################
 Interact::~Interact() {}
 
-
-
-// This calls the appropriate interaction functions in whatever object got clicked
+// #########################
+// # Interact with Objects #
+// #########################
 void Interact::interactions(int mouse_x, int mouse_y, int mouse_b, ObjectManager &OM)
 {
-	// NOTES:
-	// Nested For Loop to Check Every Item in Chunk
-	// Chunk information is pulled from Character Class
-	// Then go down to tile information (32 tiles per chunk)
-	// Analyze all objects within the Chunk associated with a tile (x, y)
-	// Apply distance formula and find the closest object
-	// Check if closest object is within clicking threshold
-	// ***What to do with items??***
-	// ***X, and Y of object indpenedent or dependent on chunks??***
-
+	// Stores Previously Calculated Distance
 	int prev_distance = 0;
+	// Stores Newly Calculated Distance
 	int new_distance = 0;
+	// Stores X Position of Object
 	int object_x = 1000000;
+	// Stores Y Position of Object
 	int object_y = 1000000;
+	// Stores Index in Array of Object
 	int object_index = 0;
 
-	// Sets Original Distance (Very High as We Want the First Object Right Away)
+	// Initializes Distance (Very High as We Want the First Object Right Away)
 	prev_distance = sqrt(pow((mouse_x - object_x), 2) + pow((mouse_y - object_y), 2));
 
-	// Loops through and finds closest item to the mouse release
+	// Loops Through the Item Layer and Finds Closest Item to the Mouse Release
 	for (int i = 0; i < OM.getIL().arrItems.size(); i++)
 	{
+		// Calculates New Distance Between Mouse and Object from Item Layer
 		new_distance = sqrt(pow((mouse_x - OM.getIL().arrItems[i].getCoordinateX()), 2) + pow((mouse_y - OM.getIL().arrItems[i].getCoordinateY()), 2));
 
+		// If the Newly Calculated Distance is Closer than the Previous ...
 		if (new_distance < prev_distance)
 		{
+			// Set the New Distance to the Old Distance
 			prev_distance = new_distance;
+			// Store the X and Y Coordinate of the Object as well as the Index in the Array
 			object_x = OM.getIL().arrItems[i].getCoordinateX();
 			object_y = OM.getIL().arrItems[i].getCoordinateY();
 			object_index = i;
 		}
 	}
 
-	// Checks if closest item is within 10 pixels of the mouse, if so, call object leftClick and rightClick function
+	// If the smallest distance found from the for loop is less than 10 ...
+	// (So if the closest object is within 10 "pixels" of the mouse)
 	if (prev_distance < 10)
 	{
+		// If the Left Button was Pressed ...
 		if (mouse == 1)
 		{
+			// Call the leftClick() Function of the Closest Object
 			OM.getIL().arrItems[object_index].leftClick();
 			return;
 		}
+		// If the Right Button was Pressed ...
 		else if (mouse == 2)
 		{
+			// Call the rightClick() Function of the Closest Object
 			OM.getIL().arrItems[object_index].rightClick();
 			return;
 		}
+		// If Somehow No Button was Pressed (Possibly Error) ...
 		else
 		{
 			return;
 		}
 	}
 
+	// Loops Through the Machine Layer and Finds Closest Machine to the Mouse Release
 	for (int j = 0; j < OM.getML().arrMachines.size(); j++)
 	{
 		new_distance = sqrt(pow((mouse_x - OM.getML().arrMachines[j].getPlacementX()), 2) + pow((mouse_y - OM.getML().arrMachines[j].getPlacementY()), 2));
@@ -95,6 +108,8 @@ void Interact::interactions(int mouse_x, int mouse_y, int mouse_b, ObjectManager
 			return;
 		}
 	}
+
+	// CURRENTLY NOT NEEDED AS NO ORE INTERACTION WILL TAKE PLACE [For Now]
 	/*
 	for (int k = 0; k < oreLayer.arrOres.size(); k++)
 	{
@@ -128,11 +143,15 @@ void Interact::interactions(int mouse_x, int mouse_y, int mouse_b, ObjectManager
 	}*/
 }
 
+// ####################################
+// # Mouse and Keyboard Functionality #
+// ####################################
 bool Interact::beginInteractions(World &Map, MainDraw &Art, ALLEGRO_DISPLAY * display, ALLEGRO_FONT * font, ALLEGRO_EVENT_QUEUE  *queue, ObjectManager &OM)
 {
+	// Waits for Event to Trigger (Mouse or Keyboard)
 	al_wait_for_event(queue, &event);
 
-	// Checks to See if Window Closed
+	// Checks to See if the Window was Closed
 	if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 	{
 		return false;
@@ -141,42 +160,64 @@ bool Interact::beginInteractions(World &Map, MainDraw &Art, ALLEGRO_DISPLAY * di
 	// Checks for Keyboard Presses
 	if (event.type == ALLEGRO_EVENT_KEY_DOWN) 
 	{
+		// If 'R' is pressed, regenerate the Terrain
 		if (event.keyboard.keycode == ALLEGRO_KEY_R)
 			Map.initialGeneration();
+
+		// If 'ESC' is pressed, quit the game
 		if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
 			return false;
 	}
 
-	// Character Movement
+	// Checks for Arrow Keys
 	if (event.type == ALLEGRO_EVENT_KEY_CHAR)
 	{
+		// If the 'UP' Arrow Key is Pressed ...
 		if (event.keyboard.keycode == ALLEGRO_KEY_UP)
 		{
+			// Move Character Up
 			movement.moveCharacterUp();
+
+			// If Character Reaches Top of Map, Reset Position to Top Coordinate
 			if (movement.getCharacterYPosition() < 0)
 			{
 				movement.setCharacterYPosition(0);
 			}
 		}
+
+		// If the 'DOWN' Arrow Key is Pressed ...
 		if (event.keyboard.keycode == ALLEGRO_KEY_DOWN)
 		{
+			// Move Character Down
 			movement.moveCharacterDown();
+
+			// If Character Reaches Bottom of Map, Reset Position to Bottom Coordinate
 			if (movement.getCharacterYPosition() > mapYBoundary)
 			{
 				movement.setCharacterYPosition(mapYBoundary);
 			}
 		}
+
+		// If the 'LEFT' Arrow Key is Pressed ...
 		if (event.keyboard.keycode == ALLEGRO_KEY_LEFT)
 		{
+			// Move Character Left
 			movement.moveCharacterLeft();
+
+			// If Character Reaches Left Boundary, Reset Position to Last Left Coordinate
 			if (movement.getCharacterXPosition() < 0)
 			{
 				movement.setCharacterXPosition(0);
 			}
 		}
+
+		// If the 'RIGHT' Arrow Key is Pressed ...
 		if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT)
 		{
+			// Move Character Right
 			movement.moveCharacterRight();
+
+			// If Character Reaches Right Boundary, Reset Position to Last Right Coordinate
 			if (movement.getCharacterXPosition() > mapXBoundary)
 			{
 				movement.setCharacterXPosition(mapXBoundary);
@@ -187,41 +228,55 @@ bool Interact::beginInteractions(World &Map, MainDraw &Art, ALLEGRO_DISPLAY * di
 	// Checks for Mouse Button Press
 	if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) 
 	{
+		// Save X and Y Coordinate of Where Mouse was Pressed
 		start_x = event.mouse.x;
 		start_y = event.mouse.y;
+
+		// Save Whether it was a Left or Right Click
 		mouse = event.mouse.button;
 	}
 
 	// Checks for Mouse Button Release
 	if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) 
 	{
+		// Save X and Y Coordinate of Where Mouse was Released
 		end_x = event.mouse.x;
 		end_y = event.mouse.y;
 
+		// Calculate Distance Between Mouse Click and Release
 		distance = sqrt(pow((end_x - start_x), 2) + pow((end_y - start_y), 2));
 
+		// If there was Less than Two "Pixels" between Mouse Positions ...
 		if (distance < 2)
 		{
+			// Run the Interactions Function
 			interactions(end_x, end_y, mouse, OM);
 		}
 
+		// Reset Mouse Button
 		mouse = 0;
 	}
 
 	// Actions for Mouse Movement
-	if (event.type == ALLEGRO_EVENT_MOUSE_AXES) {
-		/* Left button scrolls. */
-		if (mouse == 1) {
+	if (event.type == ALLEGRO_EVENT_MOUSE_AXES) 
+	{
+		
+		// If it was a Left Click, Scroll Across the Map
+		if (mouse == 1) 
+		{
 			float x = event.mouse.dx / zoom;
 			float y = event.mouse.dy / zoom;
 			scroll_x -= x * cos(rotate) + y * sin(rotate);
 			scroll_y -= y * cos(rotate) - x * sin(rotate);
 		}
-		/* Right button zooms/rotates. */
-		if (mouse == 2) {
+		
+		// If it was a Right Click, Zoom/Rotate the Map
+		if (mouse == 2) 
+		{
 			rotate += event.mouse.dx * 0.01;
 			zoom += event.mouse.dy * 0.01 * zoom;
 		}
+
 		zoom += event.mouse.dz * 0.1 * zoom;
 		if (zoom < 0.1) zoom = 0.1;
 		if (zoom > 10) zoom = 10;
@@ -232,10 +287,11 @@ bool Interact::beginInteractions(World &Map, MainDraw &Art, ALLEGRO_DISPLAY * di
 		redraw = true;
 
 	// Redraws when Window Changes Size
-	if (event.type == ALLEGRO_EVENT_DISPLAY_RESIZE) {
+	if (event.type == ALLEGRO_EVENT_DISPLAY_RESIZE) 
+	{
 		al_acknowledge_resize(display);
 		redraw = true;
 	}
+
 	return true;
 }
-
--- DevSprint1/Interact.h
+++ DevSprint1/Interact.h
@@ -1,56 +1,55 @@
 #pragma once
+//! Interactions
 
-#include <string>
+/*!
+	This allows the user to Interact with Objects and the World Itself (Zoom, Rotate, Etc.)
+*/
 
+//! Include Allegro Addons
 #include "allegro5/allegro.h"
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_primitives.h"
 #include "allegro5/allegro_font.h"
 
-#include "MainDraw.h"
-#include "World.h"
-#include "ItemBase.h"
-
-#include "ItemLayer.h"
-#include "MachineLayer.h"
-#include "OreLayer.h"
-
+//! Include Standard Libraries
+#include <string>
 #include <math.h>
 #include <vector>
 #include <array>
 
+//! Include Custom Classes
+#include "MainDraw.h"
+#include "World.h"
+#include "ItemBase.h"
+#include "ItemLayer.h"
+#include "MachineLayer.h"
+#include "OreLayer.h"
 #include "CharacterMovement.h"
 #include "ObjectManager.h"
 
+//! Interaction Class
 class Interact
 {
 public:
-	Interact();
-	//Interact(ALLEGRO_TIMER * timer, ALLEGRO_DISPLAY * display);
-	~Interact();
 
-	void interactions(int mouse_x, int mouse_y, int mouse_b, ObjectManager &OM);
-
-	bool beginInteractions(World &Map,MainDraw &Art, ALLEGRO_DISPLAY * display, ALLEGRO_FONT * font, ALLEGRO_EVENT_QUEUE  *queue, ObjectManager &OM);
-
-
-
-	/* Keep track of pressed mouse button. */
+	//! Used to Tell if Mouse was Pressed by Left or Right Button
 	int mouse;
-	/* Camera parameters. */
+
+	//! Parameters Used for Camera Effects [Will Likely be Removed]
 	float zoom = 0.5, rotate = 0.0f;
 
+	//! Used when Drawing
 	bool redraw = true;
 
-	/* Center of map. */
+	//! Gives Positioning to Center of Map [Should be Renamed and Made Constant]
 	float scroll_x = 100 * 32 / 2;
 	float scroll_y = 100 * 32 / 2;
 
-	/* Simple FPS counter. */
+	//! Counter to Show In-Game FPS
 	int fps, fps_accum;
 	double fps_time;
 
-	/* Character Position and Image DF*/
+	//! Variables for the Positioning and Movement of our Character as well as it's Image
 	int charXPosition = 0;
 	int charYPosition = 0;
 	ALLEGRO_BITMAP *character = al_load_bitmap("Terrain//Dev//test.png");
@@ -58,13 +57,35 @@ public:
 	int mapXBoundary = 6100;
 	int mapYBoundary = 6100;
 
-private:
-	//ALLEGRO_EVENT_QUEUE *queue;
-	//ALLEGRO_EVENT event;
+	//! Constructor
+	Interact();
+	
+	//! Deconstructor
+	~Interact();
 
+	//! Main Function for Interacting with Objects
+	void interactions(int mouse_x, int mouse_y, int mouse_b, ObjectManager &OM);
+
+	//! Allows Keyboard and Mouse Functionality
+	//! Parameters:
+	//! <> &Map -> Reference to our Game Map
+	//! <> &Art -> Reference to Drawing Capabilities
+	//! <> *display -> Gives access to the Window Display
+	//! <> *font -> Gives access to the Game Font
+	//! <> *queue -> Gives access to the Queue for Different Events
+	//! <> &OM -> References the Object Manager for Accessibility
+	bool beginInteractions(World &Map,MainDraw &Art, ALLEGRO_DISPLAY * display, ALLEGRO_FONT * font, ALLEGRO_EVENT_QUEUE  *queue, ObjectManager &OM);
+
+private:
+
+	//! X and Y Positioning of Initial Mouse Button Press
 	int start_x;
 	int start_y;
+
+	//! X and Y of Ending Mouse Button Release
 	int end_x;
 	int end_y;
+
+	//! Used to Store Distance Between Objects
 	float distance;
 };
